96a82af40567321320e6c89b38febbbaa870539a
Rudraksh Sachdeva <rudrakshsachdeva04@gmail.com>
2025-06-30 15:28:03 +0530

    Update settings.py

--- Portfolio_Resume_Website/settings.py
+++ Portfolio_Resume_Website/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rudrakshsachdeva22@gmail.com"
-EMAIL_HOST_PASSWORD = 'gkej nlmm nzsp zxse'
+EMAIL_HOST_USER = os.get.env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.get.env('EMAIL_HOST_PASSWORD)
 
